25e60693804b750b3ce95e857f86219628f5fb2b
Andy Chou <acchou2@gmail.com>
2017-02-16 17:14:49 -0800

    Added Google sign in button.

--- SwiftFirebaseTests/ViewController.swift
+++ SwiftFirebaseTests/ViewController.swift
@@ -7,19 +7,22 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GIDSignInUIDelegate {
+
+    @IBOutlet weak var googleSignInButton: GIDSignInButton!
+    @IBOutlet weak var statusLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+        googleSignInButton.style = .wide
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
